77186488730a2e5060fbc0b2b8c3cce0cf8ff45a
Sumaiya215 <sumaiyacse15>
2024-10-24 20:39:55 -0700

    added simple react world tour project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Countries from './components/Countries/Countries'
+
+import './App.css'
+function App() {
+  
+  return (
+    <>
+      
+      <h1> React World Tour</h1>
+      <Countries></Countries>
+     
+      
+    </>
+  )
+}
+
+export default App
